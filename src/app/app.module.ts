e9c3591ad4ccdf7cb13ea6b61d96ab1fe69f2cf7
Caleb Davenport <xcmdprompt@gmail.com>
2018-11-30 10:14:49 -0600

    introducing angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import {routing} from "./app.routing";
-import {AuthenticationService} from "./service/auth.service";
-import {ReactiveFormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
+import { routing } from "./app.routing";
+import { AuthenticationService } from "./service/auth.service";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 import { AddUserComponent } from './add-user/add-user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
-import {ListUserComponent} from "./list-user/list-user.component";
-import {UserService} from "./service/user.service";
+import { ListUserComponent } from "./list-user/list-user.component";
+import { UserService } from "./service/user.service";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CustomMaterialModule } from "./material/material.module"
 
 @NgModule({
   declarations: [
+    // Components
     AppComponent,
     LoginComponent,
     ListUserComponent,
@@ -21,12 +24,17 @@ import {UserService} from "./service/user.service";
     EditUserComponent
   ],
   imports: [
+    // Supporting modules
     BrowserModule,
     routing,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule
   ],
+  // Services
   providers: [AuthenticationService, UserService],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
