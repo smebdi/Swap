882a567e0de33f2e42cb5c41f82e86311b348626
smebdy <calebmackdaven@gmail.com>
2018-12-03 15:16:19 -0600

    fixing bad push

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   { path: 'add-user', component: AddUserComponent },
   { path: 'list-user', component: ListUserComponent },
   { path: 'edit-user', component: EditUserComponent },
-  {path : '', component : LoginComponent}
+  { path : '', component : LoginComponent }
 ];
 
 export const routing = RouterModule.forRoot(routes);
